f74e7aab596d6a8cd1baa31249bf903d106111b1
Camilo Gonzalez <camilogonzalezjpm@gmail.com>
2024-07-09 22:54:58 -0500

    Se crea el primer endpoint para introducir nuevos datos en hired_employees

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
 from routers.health import router
-from routers.create_new_employee import router_create_new_employee, router_test
+from routers.create_new_employee import router_create_new_employee
+from routers.test_new_db import router_test
 
 app = FastAPI()
 app.include_router(router)
 app.include_router(router_test)
 app.include_router(router_create_new_employee)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
--- backend/routers/create_new_employee.py
+++ backend/routers/create_new_employee.py
@@ -1,29 +1,23 @@
 from fastapi import APIRouter, HTTPException
-from routers.health import router
 from pydantic import BaseModel
 import sqlite3
 import os
 import logging
+
+logging.basicConfig(
+      level=logging.INFO,
+      format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+      datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 router_create_new_employee = APIRouter(
     prefix="/code-challenge",
     tags=["items"],
 )
 
-logging.basicConfig(
-      level=logging.INFO,
-      format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-      datefmt='%Y-%m-%d %H:%M:%S'
-  )
-
-# Crea la conexion de la API con la base de datos
-def get_db_connection():
-    logging.info(os.getcwd())
-    conn = sqlite3.connect("config/database.db")
-    return conn
 
 # Se crea el modelo de los datos esperados
-class HiredEmployeesIn(BaseModel):
+class Employee(BaseModel):
     id:int
     name:str
     datetime:str
@@ -31,61 +25,45 @@ class HiredEmployeesIn(BaseModel):
     job_id:int
 
 # Endpoint para manejar la llegada de un nuevo dato a hired_employees
-@router_create_new_employee.post('/create-new-employee', tags=['Add'])
-def create_new_employee(employee: HiredEmployeesIn):
+@router_create_new_employee.post('/create-new-employee', tags=['Add Data'])
+def create_new_employee(employee: Employee):
+    logging.info('Ejecuta: create_new_employee')
+    # Lee los datos del request
+    id = employee.id
+    name = employee.name
+    datetime = employee.datetime
+    department_id = employee.department_id
+    job_id = employee.job_id
+    logging.info('Datos de request obtenidos')
+    # return de prueba de los datos
+    # return {'id':name, 'datetime':datetime, 'department_id':department_id, 'job_id':job_id}
+
+    # Se conecta a la BD para hacer el insert
     try:
+        logging.info('Inicia conexion con la BD')
         conn = get_db_connection()
         cursor = conn.cursor()
-        
-        # Hace un INSERT de los valores obtenido por la API
-        cursor.execute("INSERT INTO transactions (id, name, datetime, department_id, job_id) VALUES (?, ?, ?, ?, ?)",
+        # Hace un INSERT de los datos obtenidos por la API
+        logging.info('Inicia INSERT a la base de datos')
+        cursor.execute("INSERT INTO hired_employees (id, name, datetime, department_id, job_id) VALUES (?, ?, ?, ?, ?)",
                        (employee.id, employee.name, employee.datetime, employee.department_id, employee.job_id))
-        conn.commit()
-
         # Devuelve el ID del registro recien creado para confirmar que fue ingresado
         employee_id = cursor.lastrowid
-
-        # Cierra la conexion con la base de datos
+        conn.commit()
         cursor.close()
         conn.close()
-        return {"message": "Se ha ingresado correctamente el registro", "employee_id": employee_id}
+        return {"message": "Se ha ingresado correctamente el registro", "employee_row_id": employee_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"No se ha podido ingresar el registro: {str(e)}")
+
+
+# Crea la conexion de la API con la base de datos
+def get_db_connection():
+    logging.info('Ejecuta: get_db_connection')
+    logging.info(f'Current working directory: {os.getcwd()}')
+    conn = sqlite3.connect("src/database.db")
+    return conn
     
 
 
 
-router_test = APIRouter(
-    prefix="/code-challenge",
-    tags=["items"],
-)
-@router_test.get('/test_cwd', tags=['Test'])
-def test_cwd():
-    cwd = os.getcwd()
-    response = os.getcwd()
-    table_response = 'No se pudo validar en la BD'
-    try:
-        logging.info('Intenta conexion con BD')
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        response = 'Se conecto a la base de datos'
-    except:
-        logging.info('No se pudo conectar con BD')
-        response = 'No se pudo conectar'
-    try:
-        # conn = get_db_connection()
-        # conn = sqlite3.connect("config/database.db")
-        logging.info('Intenta consultar las tablas de la BD')
-        # cursor = conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';") 
-        tables = cursor.fetchall()
-        tables = [table[0] for table in tables]
-        # table_response = ','.join(tables)
-        table_response = tables
-        table_response = conn
-    except:
-        logging.info('No pudo consultar las tablas de la BD')
-        table_response = f'No se pudo validar las tablas en la BD'
-    return {'cwd':cwd,
-            'response': response,
-            'table':table_response}
