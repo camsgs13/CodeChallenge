8c510e259fd9c1a63576b7250669054fb09c4b9b
Camilo Gonzalez <camilogonzalezjpm@gmail.com>
2024-07-09 23:11:01 -0500

    Se crea el endpoint para introducir nuevos datos en departments

--- backend/routers/create_new_department.py
+++ backend/routers/create_new_department.py
@@ -5,38 +5,33 @@ import os
 import logging
 
 logging.basicConfig(
-      level=logging.INFO,
-      format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-      datefmt='%Y-%m-%d %H:%M:%S'
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
 
-router_create_new_employee = APIRouter(
+router_create_new_department = APIRouter(
     prefix="/code-challenge",
     tags=["items"],
 )
 
 
 # Se crea el modelo de los datos esperados
-class Employee(BaseModel):
-    id:int
-    name:str
-    datetime:str
-    department_id:int
-    job_id:int
+class Department(BaseModel):
+    id: int
+    department: str
+
 
 # Endpoint para manejar la llegada de un nuevo dato a hired_employees
-@router_create_new_employee.post('/create-new-employee', tags=['Add Data'])
-def create_new_employee(employee: Employee):
-    logging.info('Ejecuta: create_new_employee')
+@router_create_new_department.post('/create-new-department', tags=['Add Data'])
+def create_new_department(dept: Department):
+    logging.info('Ejecuta: create_new_department')
     # Lee los datos del request
-    id = employee.id
-    name = employee.name
-    datetime = employee.datetime
-    department_id = employee.department_id
-    job_id = employee.job_id
+    id = dept.id
+    department = dept.department
     logging.info('Datos de request obtenidos')
     # return de prueba de los datos
-    # return {'id':name, 'datetime':datetime, 'department_id':department_id, 'job_id':job_id}
+    # return {'id':id, 'department':department}
 
     # Se conecta a la BD para hacer el insert
     try:
@@ -45,14 +40,14 @@ def create_new_employee(employee: Employee):
         cursor = conn.cursor()
         # Hace un INSERT de los datos obtenidos por la API
         logging.info('Inicia INSERT a la base de datos')
-        cursor.execute("INSERT INTO hired_employees (id, name, datetime, department_id, job_id) VALUES (?, ?, ?, ?, ?)",
-                       (id, name, datetime, department_id, job_id))
+        cursor.execute("INSERT INTO departments (id, department) VALUES (?, ?)",
+                       (id, department))
         # Devuelve el ID del registro recien creado para confirmar que fue ingresado
-        employee_id = cursor.lastrowid
+        dept_id = cursor.lastrowid
         conn.commit()
         cursor.close()
         conn.close()
-        return {"message": "Se ha ingresado correctamente el registro", "employee_row_id": employee_id}
+        return {"message": "Se ha ingresado correctamente el registro", "dept_row_id": dept_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"No se ha podido ingresar el registro: {str(e)}")
 
@@ -63,7 +58,3 @@ def get_db_connection():
     logging.info(f'Current working directory: {os.getcwd()}')
     conn = sqlite3.connect("src/database.db")
     return conn
-    
-
-
-
